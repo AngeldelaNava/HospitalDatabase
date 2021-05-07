44b8f090e8853a5e92362c20eaf97a09747ac805
Angel de la Nava <a.nava1@usp.ceu.es>
2021-05-07 19:07:50 +0200

    Iterator import removed

--- src/hospitaldatabase/db/ui/Menu.java
+++ src/hospitaldatabase/db/ui/Menu.java
@@ -8,7 +8,6 @@ import java.sql.Time;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Iterator;
 import java.util.List;
 
 import hospitaldatabase.db.ifaces.HospitalDBManager;
