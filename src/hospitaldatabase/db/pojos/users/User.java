ec1a6bc34aa2e2fc14855a18c756754d55ac6278
Angel de la Nava <a.nava1@usp.ceu.es>
2021-05-12 10:15:49 +0200

    Created JPA pojos

--- src/hospitaldatabase/db/pojos/users/User.java
+++ src/hospitaldatabase/db/pojos/users/User.java
@@ -1,7 +1,96 @@
 package hospitaldatabase.db.pojos.users;
 
 import java.io.Serializable;
+import java.util.Arrays;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="User")
 public class User implements Serializable {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4218782928514014807L;
+
+	@Id
+	@GeneratedValue(generator="User")
+	@TableGenerator(name="User", table="sqlite_sequence", pkColumnName="name", valueColumnName="seq", pkColumnValue="User")
+	private Integer id;
+	private String email;
+	@Lob
+	private byte[] password;
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name="role_id")
+	private Role role;
+	
+	public User(String email, byte[] password, Role role) {
+		super();
+		this.email = email;
+		this.password = password;
+		this.role = role;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public byte[] getPassword() {
+		return password;
+	}
+
+	public void setPassword(byte[] password) {
+		this.password = password;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", email=" + email + ", password=" + Arrays.toString(password) + ", role=" + role + "]";
+	}
 }
