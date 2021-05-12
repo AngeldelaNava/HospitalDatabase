653a39cd9ae000604dd609e94ce78677eb78137d
Angel de la Nava <a.nava1@usp.ceu.es>
2021-05-12 19:18:50 +0200

    Added methods to the interface and implemented (TODO)

--- src/hospitaldatabase/db/ifaces/HospitalUserManager.java
+++ src/hospitaldatabase/db/ifaces/HospitalUserManager.java
@@ -1,5 +1,17 @@
 package hospitaldatabase.db.ifaces;
 
+import java.util.List;
+
+import hospitaldatabase.db.pojos.users.Role;
+import hospitaldatabase.db.pojos.users.User;
+
 public interface HospitalUserManager {
 
+	public void connect();
+	public void disconnect();
+	public void newUser(User u);
+	public void newRole(Role r);
+	public Role getRole(int id);
+	public List<Role> getRoles();
+	public User checkPassword(String email, String password);
 }
--- src/hospitaldatabase/db/jpa/HospitalJPAUserManager.java
+++ src/hospitaldatabase/db/jpa/HospitalJPAUserManager.java
@@ -1,7 +1,53 @@
 package hospitaldatabase.db.jpa;
 
+import java.util.List;
+
 import hospitaldatabase.db.ifaces.HospitalUserManager;
+import hospitaldatabase.db.pojos.users.Role;
+import hospitaldatabase.db.pojos.users.User;
 
 public class HospitalJPAUserManager implements HospitalUserManager {
+
+	@Override
+	public void connect() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void disconnect() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void newUser(User u) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void newRole(Role r) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Role getRole(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Role> getRoles() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User checkPassword(String email, String password) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
